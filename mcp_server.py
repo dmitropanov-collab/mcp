ffbe59245f3efeb677e563131f1b6de1282cf13b
dmitropanov-collab <dmitropanov@gmail.com>
2026-03-30 21:18:40 +0300

    implemented 2 methods for mcp server

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
@@ -12,8 +13,32 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
-# TODO: Write a tool to edit a doc
+@mcp.tool(
+    name = "read_doc_contents",
+    description = "Read the contents of a document given its id"
+)
+def read_doc_contents(doc_id: str = Field(..., description="The id of the document to read")) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with id '{doc_id}' not found.")
+    return docs.get(doc_id)
+
+@mcp.tool(
+    name = "edit_docucment",
+    description = "Edit the contents of a document given its id and new contents"
+)
+def edit_document(
+    doc_id: str = Field(..., description="The id of the document to edit"),
+    old_str: str = Field(..., description="The string to be replaced in the document"),
+    new_str: str = Field(..., description="The string to replace the old string with in the document")
+) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with id '{doc_id}' not found.")
+    doc_contents = docs.get(doc_id)
+    if old_str not in doc_contents:
+        raise ValueError(f"String '{old_str}' not found in document with id '{doc_id}'.")
+    updated_contents = doc_contents.replace(old_str, new_str)
+    docs[doc_id] = updated_contents
+    return updated_contents
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
